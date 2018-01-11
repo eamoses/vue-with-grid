337142f5dd282aaf11df3fda3bad4837186cc9e6
Emily <e.a.moses@gmail.com>
2018-01-11 13:03:43 -0600

    modal functioning with bootstrap

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var app = new Vue({
       {title: "Fusce", body: "Fusce molestie..."},
       {title: "Aenean", body: "Aenean et lectus..."},
       {title: "Rhoncus", body: "Rhoncus eleifend..."},
-      {title: "Sed", body: "Sed tellus..."},
-	  {title: "Morbi", body: "Morbi nec dictum..."},
-	  {title: "Fusce", body: "Fusce molestie..."},
-	  {title: "Aenean", body: "Aenean et lectus..."},
-	  {title: "Rhoncus", body: "Rhoncus eleifend..."},
-	  {title: "Sed", body: "Sed tellus..."}
-    ]
+      {title: "Sed", body: "Sed tellus..."}
+    ],
+    currentItem: {}
+  },
+  methods: {
+    setCurrentItem: function(item) {
+      this.currentItem = item
+    }
   }
-})
+ })
