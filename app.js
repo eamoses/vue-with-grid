a12c95039a1eca36b373c3adb41e395b7fec37f3
Emily <e.a.moses@gmail.com>
2018-01-11 12:47:45 -0600

    vue repeating entered data and grid css with responsive buttons

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    items: [
+      {title: "Morbi", body: "Morbi nec dictum..."},
+      {title: "Fusce", body: "Fusce molestie..."},
+      {title: "Aenean", body: "Aenean et lectus..."},
+      {title: "Rhoncus", body: "Rhoncus eleifend..."},
+      {title: "Sed", body: "Sed tellus..."},
+	  {title: "Morbi", body: "Morbi nec dictum..."},
+	  {title: "Fusce", body: "Fusce molestie..."},
+	  {title: "Aenean", body: "Aenean et lectus..."},
+	  {title: "Rhoncus", body: "Rhoncus eleifend..."},
+	  {title: "Sed", body: "Sed tellus..."}
+    ]
+  }
+})
